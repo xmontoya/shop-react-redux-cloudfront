ef1cd8bae50db6d8f1007e3bde03a501b879bc51
Xavier Montoya <8439793+xmontoya@users.noreply.github.com>
2023-03-23 15:03:40 -0600

    Task 5: update import service urls

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://yecrj6u86i.execute-api.us-west-1.amazonaws.com/dev",
   order: "https://yecrj6u86i.execute-api.us-west-1.amazonaws.com/dev",
-  import: "https://yecrj6u86i.execute-api.us-west-1.amazonaws.com/dev",
+  import: "https://y0e2itat93.execute-api.us-west-1.amazonaws.com/dev",
   bff: "https://yecrj6u86i.execute-api.us-west-1.amazonaws.com/dev",
   cart: "https://yecrj6u86i.execute-api.us-west-1.amazonaws.com/dev",
 };
