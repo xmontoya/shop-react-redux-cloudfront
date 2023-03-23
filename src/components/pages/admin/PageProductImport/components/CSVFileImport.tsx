e0014dcc59ae55a545071ff6cca779f00941dd18
Xavier Montoya <8439793+xmontoya@users.noreply.github.com>
2023-03-23 15:19:16 -0600

    Task 5: componenets updates

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -31,17 +31,17 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
        method: "GET",
        url,
        params: {
-         name: encodeURIComponent(file.name),
+         name: encodeURIComponent(file?.name ?? ''),
        },
      });
-     console.log("File to upload: ", file.name);
+     console.log("File to upload: ", file?.name ?? '');
      console.log("Uploading to: ", response.data);
      const result = await fetch(response.data, {
        method: "PUT",
        body: file,
      });
      console.log("Result: ", result);
-     setFile("");
+     setFile(undefined);
   };
   return (
     <Box>
